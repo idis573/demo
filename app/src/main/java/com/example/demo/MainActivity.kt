a78dd7973a473f9c67aea3cc7203360d4e7ee8e4
qingshan <nlwqs573@163.com>
2020-03-19 21:27:40 +0800

    init

---
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        progress_circular.setProgress(90,100)
+        progress_circular.setProgWidth(20)
+    }
+}
